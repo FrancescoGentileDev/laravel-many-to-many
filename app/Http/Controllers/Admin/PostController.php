f4d37338c6ad5541a0a3438242127ed492e6d8f6
svalvastic <assogames2@gmail.com>
2022-11-14 15:59:29 +0100

    ✨ feat(view): list post in view

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -15,8 +15,8 @@ class PostController extends Controller
     public function index()
     {
         //
-        $post = Post::all();
-        return view('admin.post', compact($post));
+        $posts = Post::all();
+        return view('admin.post', compact('posts'));
     }
 
     /**
--- resources/views/admin/post.blade.php
+++ resources/views/admin/post.blade.php
@@ -1,3 +1,16 @@
 @extends('layouts.dashboard')
 
 
+@section('content')
+    <ul>
+
+        @foreach ($posts as $post)
+            <li>
+
+                <a href="{{ route('admin.posts.show', ['post'=> $post->id]) }}">{{ $post->title }}</a>
+
+            </li>
+        @endforeach
+
+    </ul>
+@endsection
