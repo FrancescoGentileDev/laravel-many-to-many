688be033613d1c80836559f1306db9004af8a9df
svalvastic <assogames2@gmail.com>
2022-11-14 17:38:18 +0100

    🌈 style(index): beautify index

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -2,22 +2,46 @@
 
 
 @section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>Posts</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <a class="btn btn-primary" href="{{ route('admin.posts.create') }}">Create new post</a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Title</th>
+                            <th colspan="3">Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($posts as $post)
+                            <tr>
+                                <td>{{ $post->id }}</td>
+                                <td>{{ $post->title }}</td>
+                                <td><a class="btn btn-primary" href="{{ route('admin.posts.show', ['post' => $post->slug]) }}">View</a></td>
+                                <td><a class="btn btn-warning" href="{{ route('admin.posts.edit', ['post' => $post->id]) }}">Edit</a></td>
+                                <td>
+                                    <form action="{{ route('admin.posts.destroy', ['post' => $post->id]) }}" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        <input class="btn btn-danger" type="submit" value="Delete">
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
     <ul>
-
-        @foreach ($posts as $post)
-            <li class="mt-3">
-
-                <a href="{{ route('admin.posts.show', ['post' => $post->slug]) }}">{{ $post->title }}</a>
-                <a class="btn btn-primary" href="{{ route('admin.posts.edit', ['post' => $post->id]) }}">Edit</a>
-
-                <form action="{{ route('admin.posts.destroy', ['post' => $post->id]) }}" method="POST" class="d-inline">
-                    @csrf
-                    @method('DELETE')
-                    <input class="btn btn-primary" type="submit" value="Delete">
-                </form>
-
-            </li>
-        @endforeach
-
-    </ul>
 @endsection
